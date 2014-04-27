b854c86c7cc9293d41f99ff921017a4d18484f61
TsTraining <thorsten.klampfl@gmail.com>
2014-04-27 23:28:19 +0100

    Programming Assignment 2 - solved

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
 ## Two functions to calculate and cache the result of a matrix inversion
 
 ## The following function creates a list of functions that provide setting (set) and
-getting (get) a matrix and setting (setinv) and getting (getinv) the inversion of that matrix
+## getting (get) a matrix and setting (setinv) and getting (getinv) the inversion of that
+## matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 	inv <- NULL
@@ -19,8 +20,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## This function takes the returned value of the first function and either calculates
-the inversion of the matrix and caches the result, or - if available - it retrieves the
-cached result of a previous inversion calculation for that matrix.
+## the inversion of the matrix and caches the result, or - if available - it retrieves the
+## cached result of a previous inversion calculation for that matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
